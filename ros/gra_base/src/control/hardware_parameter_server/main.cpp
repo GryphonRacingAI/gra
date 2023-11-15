0efe49d4e72ffc0e3e84df92b9338804e8b8d9f2
ugsfume <86786290+ugsfume@users.noreply.github.com>
2023-11-15 23:14:35 +0000

    initial commit

--- ros/gra_base/src/control/hardware_parameter_server/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "./hardware_parameter_server.h"
-
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "hardware_parameter_server");
-    // Create an instance of ControlParameterServer
-    HardwareParameterServer paramServer;
-    ros::spin();
-    return 0;
-}
