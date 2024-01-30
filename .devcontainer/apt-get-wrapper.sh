5f906236a7eaa8e93dffcac929658f6c721ed32c
ugsfume <86786290+ugsfume@users.noreply.github.com>
2024-01-30 19:41:17 +0000

    intel realsense package installed

--- .devcontainer/apt-get-wrapper.sh
+++ .devcontainer/apt-get-wrapper.sh
@@ -3,13 +3,24 @@
 DAY_IN_SECONDS=86400
 FILE="/var/cache/apt/pkgcache.bin"
 
-if [ ! -f "$FILE" ]; then
+function update_packages()
+ {
+    sudo mkdir -p /etc/apt/keyrings
+    curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
+    echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
+    sudo tee /etc/apt/sources.list.d/librealsense.list
+    sudo apt-get update
+
     apt-get update
+}
+
+if [ ! -f "$FILE" ]; then
+    update_packages
 else
     CACHE_AGE=$(($(date +%s) - $(stat -c %Y "$FILE")))
     
     if [ "$CACHE_AGE" -gt "$DAY_IN_SECONDS" ]; then
-        apt-get update
+        update_packages
     fi
 fi
 
