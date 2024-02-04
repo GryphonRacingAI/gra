122f2bf6a37f52ba502ee8aa1861a2062d58fd10
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-04 21:19:00 +0000

    Do not generate keys

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,12 +1,12 @@
 # Make link from ~/gra/ros to ~/catkin_ws/src
 ln -s ~/gra/ros ~/catkin_ws/src
 
-# Generate ssh keys if they don't exist at ~/.ssh/id_rsa
-if [ ! -f ~/.ssh/id_rsa ]; then
-    echo -e "\e[36mGenerating ssh keys...\e[0m"
-    ssh-keygen -t rsa -b 4096 -C "gra@devcontainer" -f ~/.ssh/id_rsa -q -N ""
-    echo -e "\e[36mSSH keys generated.\e[0m"
-fi
+# # Generate ssh keys if they don't exist at ~/.ssh/id_rsa
+# if [ ! -f ~/.ssh/id_rsa ]; then
+#     echo -e "\e[36mGenerating ssh keys...\e[0m"
+#     ssh-keygen -t rsa -b 4096 -C "gra@devcontainer" -f ~/.ssh/id_rsa -q -N ""
+#     echo -e "\e[36mSSH keys generated.\e[0m"
+# fi
 
 # Add ssh keys to ssh-agent
 ssh-add ~/.ssh/id_rsa
