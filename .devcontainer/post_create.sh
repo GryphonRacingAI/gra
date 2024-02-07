26389ab4846fdc9ea1edfa3e464e6e4c59e17241
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-07 00:44:10 +0000

    Create devcontainer.metadata automatically

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,15 +1,21 @@
 # Make link from ~/gra/ros to ~/catkin_ws/src
 ln -s ~/gra/ros ~/catkin_ws/src
 
-# Generate ssh keys if they don't exist at ~/.ssh/id_rsa
-if [ ! -f ~/.ssh/id_rsa ]; then
-    echo -e "\e[36mGenerating ssh keys...\e[0m"
-    ssh-keygen -t rsa -b 4096 -C "gra@devcontainer" -f ~/.ssh/id_rsa -q -N ""
-    echo -e "\e[36mSSH keys generated.\e[0m"
-fi
+# # Generate ssh keys if they don't exist at ~/.ssh/id_rsa
+# if [ ! -f ~/.ssh/id_rsa ]; then
+#     echo -e "\e[36mGenerating ssh keys...\e[0m"
+#     ssh-keygen -t rsa -b 4096 -C "gra@devcontainer" -f ~/.ssh/id_rsa -q -N ""
+#     echo -e "\e[36mSSH keys generated.\e[0m"
+# fi
 
-# Add ssh keys to ssh-agent
-ssh-add ~/.ssh/id_rsa
+# # Add ssh keys to ssh-agent
+# ssh-add ~/.ssh/id_rsa
+
+# Create devcontainer.metadata from template if it doesn't exist
+if [ ! -f ~/gra/.devcontainer/devcontainer.metadata ]; then
+    echo -e "\e[36mCreating devcontainer.metadata ...\e[0m"
+    cp ~/gra/.devcontainer/devcontainer.metadata_template ~/gra/.devcontainer/devcontainer.metadata
+fi
 
 # Run acm
 bash -ic "source ~/.bashrc && setdevmaster; cb && abl"
