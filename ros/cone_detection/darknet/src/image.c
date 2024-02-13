8bb95d466c5637a3498c8940b67e574f64aea7ac
ugsfume <86786290+ugsfume@users.noreply.github.com>
2024-02-13 23:28:27 +0000

    disabled docker extensions

--- ros/cone_detection/darknet/src/image.c
+++ ros/cone_detection/darknet/src/image.c
@@ -13,11 +13,11 @@
 
 #ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
-#include "../3rdparty/stb/include/stb_image.h"
+#include "stb_image.h"
 #endif
 #ifndef STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../3rdparty/stb/include/stb_image_write.h"
+#include "stb_image_write.h"
 #endif
 
 extern int check_mistakes;
