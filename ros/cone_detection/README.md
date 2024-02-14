bcba581afb272cf8fc6a155b1576179c50ce84f3
ugsfume <86786290+ugsfume@users.noreply.github.com>
2024-02-14 13:21:10 +0000

    initial commit

--- ros/cone_detection/README.md
+++ ros/cone_detection/README.md
@@ -1,51 +1,114 @@
-# YOLOv4 ROS
+# ultralytics_ros [![ROS-noetic Industrial CI](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/noetic-ci.yml/badge.svg)](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/noetic-ci.yml) [![ROS-noetic Docker Build Check](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/noetic-docker-build-check.yml/badge.svg)](https://github.com/Alpaca-zip/ultralytics_ros/actions/workflows/noetic-docker-build-check.yml)
+ROS package for real-time object detection and segmentation using the Ultralytics YOLO, enabling flexible integration with various robotics applications.
 
-## Overview
+|  `tracker_node`  |  `tracker_with_cloud_node`  |
+| :------------: | :-----------------------: |
+| <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/7ccefee5-1bf9-48de-97e0-a61000bba822" width="450px"> | <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/674f352f-5171-4fcf-beb5-394aa3dfe320" height="160px"> |
 
-This is a ROS package for YOLOv4. It is modified from the repository https://github.com/leggedrobotics/darknet_ros/. It has been tested on ROS Noetic.
+- The `tracker_node` provides real-time object detection and segmentation on incoming ROS image messages using the Ultralytics YOLO model.
+- The `tracker_with_cloud_node` provides functionality for 3D object detection by integrating 2D detections, mask image, LiDAR data, and camera information.
 
-## Installation
+## Setup ⚙
+```
+$ cd ~/catkin_ws/src
+$ GIT_LFS_SKIP_SMUDGE=1 git clone -b noetic-devel https://github.com/Alpaca-zip/ultralytics_ros.git
+$ python3 -m pip install -r ultralytics_ros/requirements.txt
+$ cd ~/catkin_ws
+$ rosdep install -r -y -i --from-paths .
+$ catkin build
+```
+**NOTE**: If you want to download KITTI datasets, remove `GIT_LFS_SKIP_SMUDGE=1` from the command line.
+## Run 🚀
+**`tracker_node`**
+```
+$ roslaunch ultralytics_ros tracker.launch debug:=true
+```
+**`tracker_node` & `tracker_with_cloud_node`**
+```
+$ roslaunch ultralytics_ros tracker_with_cloud.launch debug:=true
+```
+**NOTE**: If the 3D bounding box is not displayed correctly, please consider using a lighter yolo model(`yolov8n.pt`) or increasing the `voxel_leaf_size`.
 
-In order to install darknet_ros, clone the latest version from this repository into your catkin workspace and compile the package using ROS.
+## `tracker_node`
+### Params
+- `yolo_model`: Pre-trained Weights.  
+For yolov8, you can choose `yolov8*.pt`, `yolov8*-seg.pt`.
 
-    cd yolov4-ws/src
-    git clone --recursive https://github.com/t1mkhuan9/yolov4-ros-noetic darknet_ros
+  |  YOLOv8  |  <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/08770080-bf20-470b-8269-eee7a7c41acc" width="350px">  |
+  | :-------------: | :-------------: |
+  |  **YOLOv8-seg**  |  <img src="https://github.com/Alpaca-zip/ultralytics_ros/assets/84959376/7bb6650c-769d-41c1-86f7-39fcbf01bc7c" width="350px">  |
+  
+  See also: https://docs.ultralytics.com/models/
+- `input_topic`: Topic name for input image.
+- `result_topic`: Topic name of the custom message containing the 2D bounding box and the mask image.
+- `result_image_topic`: Topic name of the image on which the detection and segmentation results are plotted.
+- `conf_thres`: Confidence threshold below which boxes will be filtered out.
+- `iou_thres`: IoU threshold below which boxes will be filtered out during NMS.
+- `max_det`: Maximum number of boxes to keep after NMS.
+- `tracker`: Tracking algorithms.
+- `device`: Device to run the model on(e.g. cpu or cuda:0).
+  ```xml
+  <arg name="device" default="cpu"/> <!-- cpu -->
+  ```
+  ```xml
+  <arg name="device" default="0"/> <!-- cuda:0 -->
+  ```
+- `classes`: List of class indices to consider.
+  ```xml
+  <arg name="classes" default="[0, 1]"/> <!-- person, bicycle -->
+  ```
+  See also: https://github.com/ultralytics/ultralytics/blob/main/ultralytics/datasets/coco128.yaml 
+- `result_conf`:  Whether to plot the detection confidence score.
+- `result_line_width`: Line width of the bounding boxes.
+- `result_font_size`: Font size of the text.
+- `result_labels`: Font to use for the text.
+- `result_font`: Whether to plot the label of bounding boxes.
+- `result_boxes`: Whether to plot the bounding boxes.
+### Topics
+- Subscribed Topics:
+  - Image data from `input_topic` parameter. ([sensor_msgs/Image](https://docs.ros.org/en/api/sensor_msgs/html/msg/Image.html))
+- Published Topics:
+  - Plotted images to `result_image_topic` parameter. ([sensor_msgs/Image](https://docs.ros.org/en/api/sensor_msgs/html/msg/Image.html))
+  - Detected objects(2D bounding box, mask image) to `result_topic` parameter. (ultralytics_ros/YoloResult)
+    ```
+    std_msgs/Header header
+    vision_msgs/Detection2DArray detections
+    sensor_msgs/Image[] masks
+    ```
+## `tracker_with_cloud_node`
+### Params
+- `camera_info_topic`: Topic name for camera info.
+- `lidar_topic`: Topic name for lidar.
+- `yolo_result_topic`: Topic name of the custom message containing the 2D bounding box and the mask image.
+- `yolo_3d_result_topic`: Topic name for 3D bounding box.
+- `cluster_tolerance`: Spatial cluster tolerance as a measure in the L2 Euclidean space.
+- `voxel_leaf_size`: Voxel size for pointcloud downsampling.
+- `min_cluster_size`: Minimum number of points that a cluster needs to contain.
+- `max_cluster_size`: Maximum number of points that a cluster needs to contain.
+### Topics
+- Subscribed Topics:
+  - Camera info from `camera_info_topic` parameter. ([sensor_msgs/CameraInfo](https://docs.ros.org/en/api/sensor_msgs/html/msg/CameraInfo.html))
+  - Lidar data from `lidar_topic` parameter. ([sensor_msgs/PointCloud2](https://docs.ros.org/en/api/sensor_msgs/html/msg/PointCloud2.html))
+  - Detected objects(2D bounding box, mask image) from `yolo_result_topic` parameter. (ultralytics_ros/YoloResult)
+    ```
+    std_msgs/Header header
+    vision_msgs/Detection2DArray detections
+    sensor_msgs/Image[] masks
+    ```
+- Published Topics:
+  - Detected cloud points to `/detection_cloud` topic. ([sensor_msgs/PointCloud2](https://docs.ros.org/en/api/sensor_msgs/html/msg/PointCloud2.html))
+  - Detected objects(3D bounding box) to `yolo_3d_result_topic` parameter. ([vision_msgs/Detection3DArray](http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/Detection3DArray.html))
+  - Visualization markers to `/detection_marker` topic. ([visualization_msgs/MarkerArray](https://docs.ros.org/en/api/visualization_msgs/html/msg/MarkerArray.html))
+## Docker with KITTI datasets 🐳
+[![dockeri.co](https://dockerico.blankenship.io/image/alpacazip/ultralytics_ros)](https://hub.docker.com/r/alpacazip/ultralytics_ros)
 
-Then compile the package by
-
-    cd ../
-    catkin_make 
-
-### Common Fixes
-If you are compiling with CUDA, you might receive the following build error:
-
-    nvcc fatal : Unsupported gpu architecture 'compute_61'.
-
-This means that you need to check the compute capability (version) of your GPU. You can find a list of supported GPUs in CUDA here: [CUDA - WIKIPEDIA](https://en.wikipedia.org/wiki/CUDA#Supported_GPUs). Simply find the compute capability of your GPU and add it into darknet_ros/CMakeLists.txt. Simply add a similar line like
-
-    -O3 -gencode arch=compute_62,code=sm_62
-
-This build error may also occur sometimes:
-
-    image.c:16:10: fatal error: stb_image.h: No such file or directory
-        #include "stb_image.h"
-
-This is because it is not correctly referenced. Change the include statement in `darknet/src/image.c` line 16 and line 20 to
-
-    #include "../3rdparty/stb/include/stb_image.h"
-    #include "../3rdparty/stb/include/stb_image_write.h"
-
-Notice: DO NOT change if the problem does not occur.
-
-### Download weights
-
-    cd darknet_ros/yolo_network_config/weights/
-    wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
-    wget https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov4-tiny.weights
-
-
-## Basic Usage
-
-    source devel/setup.zsh # or setup.bash
-    roslaunch darknet_ros yolov4.launch
-    roslaunch darknet_ros yolov4-tiny.launch
+### Docker Pull & Run
+```
+$ docker pull alpacazip/ultralytics_ros:noetic
+$ docker run -p 6080:80 --shm-size=512m alpacazip/ultralytics_ros:noetic
+```
+### Run tracker_node & tracker_with_cloud_node
+```
+$ roslaunch ultralytics_ros kitti_tracker_with_cloud.launch
+$ cd ~/catkin_ws/src/ultralytics_ros/rosbag && rosbag play kitti_2011_09_26_drive_0106_synced.bag --clock --loop
+```
---
+++ ros/cone_detection/src/tracker_with_cloud_node.cpp
@@ -0,0 +1,310 @@
+/**
+ * ultralytics_ros
+ * Copyright (C) 2023-2024  Alpaca-zip
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tracker_with_cloud_node/tracker_with_cloud_node.h"
+
+TrackerWithCloudNode::TrackerWithCloudNode() : pnh_("~")
+{
+  pnh_.param<std::string>("camera_info_topic", camera_info_topic_, "camera_info");
+  pnh_.param<std::string>("lidar_topic", lidar_topic_, "points_raw");
+  pnh_.param<std::string>("yolo_result_topic", yolo_result_topic_, "yolo_result");
+  pnh_.param<std::string>("yolo_3d_result_topic", yolo_3d_result_topic_, "yolo_3d_result");
+  pnh_.param<float>("cluster_tolerance", cluster_tolerance_, 0.5);
+  pnh_.param<float>("voxel_leaf_size", voxel_leaf_size_, 0.5);
+  pnh_.param<int>("min_cluster_size", min_cluster_size_, 100);
+  pnh_.param<int>("max_cluster_size", max_cluster_size_, 25000);
+
+  detection_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("detection_cloud", 1);
+  detection3d_pub_ = nh_.advertise<vision_msgs::Detection3DArray>(yolo_3d_result_topic_, 1);
+  marker_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("detection_marker", 1);
+  camera_info_sub_.subscribe(nh_, camera_info_topic_, 10);
+  lidar_sub_.subscribe(nh_, lidar_topic_, 10);
+  yolo_result_sub_.subscribe(nh_, yolo_result_topic_, 10);
+  sync_ = boost::make_shared<message_filters::Synchronizer<ApproximateSyncPolicy>>(1);
+  sync_->connectInput(camera_info_sub_, lidar_sub_, yolo_result_sub_);
+  sync_->registerCallback(boost::bind(&TrackerWithCloudNode::syncCallback, this, _1, _2, _3));
+
+  tf_buffer_.reset(new tf2_ros::Buffer(ros::Duration(2.0), true));
+  tf_listener_.reset(new tf2_ros::TransformListener(*tf_buffer_));
+}
+
+void TrackerWithCloudNode::syncCallback(const sensor_msgs::CameraInfo::ConstPtr& camera_info_msg,
+                                        const sensor_msgs::PointCloud2ConstPtr& cloud_msg,
+                                        const ultralytics_ros::YoloResultConstPtr& yolo_result_msg)
+{
+  ros::Time current_call_time = ros::Time::now();
+  ros::Duration callback_interval = current_call_time - last_call_time_;
+  last_call_time_ = current_call_time;
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud;
+  vision_msgs::Detection3DArray detections3d_msg;
+  sensor_msgs::PointCloud2 detection_cloud_msg;
+  visualization_msgs::MarkerArray marker_array_msg;
+
+  cam_model_.fromCameraInfo(camera_info_msg);
+  transformed_cloud = msg2TransformedCloud(cloud_msg);
+  projectCloud(transformed_cloud, yolo_result_msg, cloud_msg->header, detections3d_msg, detection_cloud_msg);
+  marker_array_msg = createMarkerArray(detections3d_msg, callback_interval.toSec());
+
+  detection3d_pub_.publish(detections3d_msg);
+  detection_cloud_pub_.publish(detection_cloud_msg);
+  marker_pub_.publish(marker_array_msg);
+}
+
+void TrackerWithCloudNode::projectCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
+                                        const ultralytics_ros::YoloResultConstPtr& yolo_result_msg,
+                                        const std_msgs::Header& header, vision_msgs::Detection3DArray& detections3d_msg,
+                                        sensor_msgs::PointCloud2& combine_detection_cloud_msg)
+{
+  pcl::PointCloud<pcl::PointXYZ> combine_detection_cloud;
+  detections3d_msg.header = header;
+  detections3d_msg.header.stamp = yolo_result_msg->header.stamp;
+
+  for (size_t i = 0; i < yolo_result_msg->detections.detections.size(); i++)
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr detection_cloud_raw(new pcl::PointCloud<pcl::PointXYZ>);
+
+    if (yolo_result_msg->masks.empty())
+    {
+      processPointsWithBbox(cloud, yolo_result_msg->detections.detections[i], detection_cloud_raw);
+    }
+    else
+    {
+      processPointsWithMask(cloud, yolo_result_msg->masks[i], detection_cloud_raw);
+    }
+
+    if (!detection_cloud_raw->points.empty())
+    {
+      pcl::PointCloud<pcl::PointXYZ>::Ptr detection_cloud = cloud2TransformedCloud(detection_cloud_raw, header);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr closest_detection_cloud = euclideanClusterExtraction(detection_cloud);
+      createBoundingBox(detections3d_msg, closest_detection_cloud, yolo_result_msg->detections.detections[i].results);
+      combine_detection_cloud += *closest_detection_cloud;
+    }
+  }
+
+  pcl::toROSMsg(combine_detection_cloud, combine_detection_cloud_msg);
+  combine_detection_cloud_msg.header = header;
+}
+
+void TrackerWithCloudNode::processPointsWithBbox(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
+                                                 const vision_msgs::Detection2D& detection,
+                                                 pcl::PointCloud<pcl::PointXYZ>::Ptr& detection_cloud_raw)
+{
+  for (const auto& point : cloud->points)
+  {
+    cv::Point3d pt_cv(point.x, point.y, point.z);
+    cv::Point2d uv = cam_model_.project3dToPixel(pt_cv);
+    if (point.z > 0 && uv.x > 0 && uv.x >= detection.bbox.center.x - detection.bbox.size_x / 2 &&
+        uv.x <= detection.bbox.center.x + detection.bbox.size_x / 2 &&
+        uv.y >= detection.bbox.center.y - detection.bbox.size_y / 2 &&
+        uv.y <= detection.bbox.center.y + detection.bbox.size_y / 2)
+    {
+      detection_cloud_raw->points.push_back(point);
+    }
+  }
+}
+
+void TrackerWithCloudNode::processPointsWithMask(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
+                                                 const sensor_msgs::Image& mask,
+                                                 pcl::PointCloud<pcl::PointXYZ>::Ptr& detection_cloud_raw)
+{
+  cv_bridge::CvImagePtr cv_ptr;
+
+  try
+  {
+    cv_ptr = cv_bridge::toCvCopy(mask, sensor_msgs::image_encodings::MONO8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  for (const auto& point : cloud->points)
+  {
+    cv::Point3d pt_cv(point.x, point.y, point.z);
+    cv::Point2d uv = cam_model_.project3dToPixel(pt_cv);
+
+    if (point.z > 0 && uv.x >= 0 && uv.x < cv_ptr->image.cols && uv.y >= 0 && uv.y < cv_ptr->image.rows)
+    {
+      if (cv_ptr->image.at<uchar>(cv::Point(uv.x, uv.y)) > 0)
+      {
+        detection_cloud_raw->points.push_back(point);
+      }
+    }
+  }
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr
+TrackerWithCloudNode::msg2TransformedCloud(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  sensor_msgs::PointCloud2 transformed_cloud_msg;
+
+  try
+  {
+    geometry_msgs::TransformStamped tf =
+        tf_buffer_->lookupTransform(cam_model_.tfFrame(), cloud_msg->header.frame_id, cloud_msg->header.stamp);
+    pcl_ros::transformPointCloud(cam_model_.tfFrame(), *cloud_msg, transformed_cloud_msg, *tf_buffer_);
+    pcl::fromROSMsg(transformed_cloud_msg, *transformed_cloud);
+  }
+  catch (tf2::TransformException& e)
+  {
+    ROS_WARN("%s", e.what());
+  }
+
+  return transformed_cloud;
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr
+TrackerWithCloudNode::cloud2TransformedCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
+                                             const std_msgs::Header& header)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+  try
+  {
+    geometry_msgs::TransformStamped tf =
+        tf_buffer_->lookupTransform(header.frame_id, cam_model_.tfFrame(), header.stamp);
+    pcl_ros::transformPointCloud(*cloud, *transformed_cloud, tf.transform);
+  }
+  catch (tf2::TransformException& e)
+  {
+    ROS_WARN("%s", e.what());
+  }
+
+  return transformed_cloud;
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr
+TrackerWithCloudNode::euclideanClusterExtraction(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
+  voxel_grid.setInputCloud(cloud);
+  voxel_grid.setLeafSize(voxel_leaf_size_, voxel_leaf_size_, voxel_leaf_size_);
+  voxel_grid.filter(*downsampled_cloud);
+
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+  std::vector<pcl::PointIndices> cluster_indices;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+
+  ec.setClusterTolerance(cluster_tolerance_);
+  ec.setMinClusterSize(min_cluster_size_);
+  ec.setMaxClusterSize(max_cluster_size_);
+  ec.setSearchMethod(tree);
+  ec.setInputCloud(downsampled_cloud);
+  ec.extract(cluster_indices);
+
+  float min_distance = std::numeric_limits<float>::max();
+  pcl::PointCloud<pcl::PointXYZ>::Ptr closest_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+
+  for (const auto& cluster : cluster_indices)
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+    for (const auto& indice : cluster.indices)
+    {
+      cloud_cluster->push_back((*downsampled_cloud)[indice]);
+    }
+
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid(*cloud_cluster, centroid);
+    float distance = centroid.norm();
+
+    if (distance < min_distance)
+    {
+      min_distance = distance;
+      *closest_cluster = *cloud_cluster;
+    }
+  }
+
+  return closest_cluster;
+}
+
+void TrackerWithCloudNode::createBoundingBox(
+    vision_msgs::Detection3DArray& detections3d_msg, const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
+    const std::vector<vision_msgs::ObjectHypothesisWithPose>& detections_results)
+{
+  vision_msgs::Detection3D detection3d;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointXYZ min_pt, max_pt;
+  Eigen::Vector4f centroid;
+  pcl::compute3DCentroid(*cloud, centroid);
+  double theta = -atan2(centroid[1], sqrt(pow(centroid[0], 2) + pow(centroid[2], 2)));
+
+  Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+  transform.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
+  pcl::transformPointCloud(*cloud, *transformed_cloud, transform);
+
+  pcl::getMinMax3D(*transformed_cloud, min_pt, max_pt);
+  Eigen::Vector4f transformed_bbox_center =
+      Eigen::Vector4f((min_pt.x + max_pt.x) / 2, (min_pt.y + max_pt.y) / 2, (min_pt.z + max_pt.z) / 2, 1);
+  Eigen::Vector4f bbox_center = transform.inverse() * transformed_bbox_center;
+  Eigen::Quaternionf q(transform.inverse().rotation());
+
+  detection3d.bbox.center.position.x = bbox_center[0];
+  detection3d.bbox.center.position.y = bbox_center[1];
+  detection3d.bbox.center.position.z = bbox_center[2];
+  detection3d.bbox.center.orientation.x = q.x();
+  detection3d.bbox.center.orientation.y = q.y();
+  detection3d.bbox.center.orientation.z = q.z();
+  detection3d.bbox.center.orientation.w = q.w();
+  detection3d.bbox.size.x = max_pt.x - min_pt.x;
+  detection3d.bbox.size.y = max_pt.y - min_pt.y;
+  detection3d.bbox.size.z = max_pt.z - min_pt.z;
+  detection3d.results = detections_results;
+  detections3d_msg.detections.push_back(detection3d);
+}
+
+visualization_msgs::MarkerArray
+TrackerWithCloudNode::createMarkerArray(const vision_msgs::Detection3DArray& detections3d_msg, const double& duration)
+{
+  visualization_msgs::MarkerArray marker_array;
+  for (size_t i = 0; i < detections3d_msg.detections.size(); i++)
+  {
+    if (std::isfinite(detections3d_msg.detections[i].bbox.size.x) &&
+        std::isfinite(detections3d_msg.detections[i].bbox.size.y) &&
+        std::isfinite(detections3d_msg.detections[i].bbox.size.z))
+    {
+      visualization_msgs::Marker marker;
+      marker.header = detections3d_msg.header;
+      marker.ns = "detection";
+      marker.id = i;
+      marker.type = visualization_msgs::Marker::CUBE;
+      marker.action = visualization_msgs::Marker::ADD;
+      marker.pose = detections3d_msg.detections[i].bbox.center;
+      marker.scale.x = detections3d_msg.detections[i].bbox.size.x;
+      marker.scale.y = detections3d_msg.detections[i].bbox.size.y;
+      marker.scale.z = detections3d_msg.detections[i].bbox.size.z;
+      marker.color.r = 0.0;
+      marker.color.g = 1.0;
+      marker.color.b = 0.0;
+      marker.color.a = 0.5;
+      marker.lifetime = ros::Duration(duration);
+      marker_array.markers.push_back(marker);
+    }
+  }
+  return marker_array;
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "tracker_with_cloud_node");
+  TrackerWithCloudNode node;
+  ros::spin();
+}
