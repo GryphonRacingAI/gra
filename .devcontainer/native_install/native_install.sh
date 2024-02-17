33db674d4fd30d545cd4fe58cb15a0b671d154ad
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 03:14:50 +0000

    Separate sudo script

--- .devcontainer/native_install/native_install.sh
+++ .devcontainer/native_install/native_install.sh
@@ -1,7 +1,3 @@
-python3 .devcontainer/native_install/docker2sh.py .devcontainer/Dockerfile > installation_script_generated.sh
-cp .devcontainer/apt-get-wrapper.sh /usr/local/bin
-chmod +x /usr/local/bin/apt-get-wrapper.sh
-chmod +x installation_script_generated.sh
-./installation_script_generated.sh
-mkdir $HOME/catkin_ws
+#!/bin/bash
+mkdir -p $HOME/catkin_ws
 ./bot_scripts/install.sh
