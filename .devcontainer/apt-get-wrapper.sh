2d5b4b0c06d8c60321228eee7131d465990772da
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 01:47:48 +0000

    Move realsense source back to sh

--- .devcontainer/apt-get-wrapper.sh
+++ .devcontainer/apt-get-wrapper.sh
@@ -4,7 +4,13 @@ DAY_IN_SECONDS=86400
 FILE="/var/cache/apt/pkgcache.bin"
 
 function update_packages()
-{    
+{   
+    # Add RealSense source
+    mkdir -p /etc/apt/keyrings
+    curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
+    echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
+    tee /etc/apt/sources.list.d/librealsense.list
+    
     apt-get update
 }
 
