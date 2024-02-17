b1bff83d6c013a62383177494a77d70598c458d2
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 01:17:29 +0000

    Move source to dockerfile

--- .devcontainer/apt-get-wrapper.sh
+++ .devcontainer/apt-get-wrapper.sh
@@ -4,20 +4,14 @@ DAY_IN_SECONDS=86400
 FILE="/var/cache/apt/pkgcache.bin"
 
 function update_packages()
- {
-    # Add RealSense source
-    mkdir -p /etc/apt/keyrings
-    curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
-    echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
-    tee /etc/apt/sources.list.d/librealsense.list
-    
+{    
     apt-get update
 }
 
 if [ ! -f "$FILE" ] | [ ! "$(ls -A /var/lib/apt/lists)" ]; then
     update_packages
 else
-    CACHE_AGE=$(($(date +%s) - $(stat -c %Y "$FILE")))
+    CACHE_AGE=$($(date +%s) - $(stat -c %Y "$FILE"))
     
     if [ "$CACHE_AGE" -gt "$DAY_IN_SECONDS" ]; then
         update_packages
