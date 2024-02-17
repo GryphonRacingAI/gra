0e8ee266a6cc2a012c6c2012500d9243f0bb3008
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 13:29:42 +0000

    Add checks

--- .devcontainer/native_install/native_install.sh
+++ .devcontainer/native_install/native_install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+
 export USERNAME=$USER
+if [ $EUID -eq 0 ]; then
+    echo "This script needs to be run as a regular user, without sudo."
+    exit 1
+fi
+
+if ! sudo true; then
+    echo "This scrpt needs to run some commands as sudo, so it needs you to authenticate."
+    exit 1
+fi
+
 # if sudo grep -q $USERNAME /etc/sudoers
 # then
 #     echo "$USERNAME is already registered as NOPASSWD"
 # else
 #     sudo echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 # fi
-if  sudo true -neq 0; then 
-    echo "This script needs to be run as a regular account but it needs sudo for some commands."
-    exit 1;
-fi
+
 python3 .devcontainer/native_install/docker2sh.py --target gra-base .devcontainer/Dockerfile > installation_script_generated.sh
 sudo cp .devcontainer/apt-get-wrapper.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/apt-get-wrapper.sh
