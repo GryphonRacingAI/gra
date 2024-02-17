0fbb866a8f37c76954fef54663c4dc29955beeb8
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 01:16:08 +0000

    bot scripts/install.sh

--- .devcontainer/native_install/native_install.sh
+++ .devcontainer/native_install/native_install.sh
@@ -3,3 +3,5 @@ cp .devcontainer/apt-get-wrapper.sh /usr/local/bin
 chmod +x /usr/local/bin/apt-get-wrapper.sh
 chmod +x installation_script_generated.sh
 ./installation_script_generated.sh
+mkdir $HOME/catkin_ws
+source bot_scripts/install.sh
