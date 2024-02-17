f73e6dcd77bddf23b356d98a6e81281be1f572eb
Daniel Bara <a@b>
2024-02-17 17:52:24 +0000

    Uncomment devcontainer.metadata (it needs further testing)

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,18 +3,18 @@ if [ ! -d "$HOME/catkin_ws/src" ]; then
     ln -s ~/gra/ros ~/catkin_ws/src
 fi
 
-# # # Generate ssh keys if they don't exist at ~/.ssh/id_rsa
-# # if [ ! -f ~/.ssh/id_rsa ]; then
-# #     echo -e "\e[36mGenerating ssh keys...\e[0m"
-# #     ssh-keygen -t rsa -b 4096 -C "gra@devcontainer" -f ~/.ssh/id_rsa -q -N ""
-# #     echo -e "\e[36mSSH keys generated.\e[0m"
-# # fi
-
-# # Create devcontainer.metadata from template if it doesn't exist
-# if [ ! -f ~/gra/.devcontainer/devcontainer.metadata ]; then
-#     echo -e "\e[36mCreating devcontainer.metadata ...\e[0m"
-#     cp ~/gra/.devcontainer/devcontainer.metadata_template ~/gra/.devcontainer/devcontainer.metadata
+# # Generate ssh keys if they don't exist at ~/.ssh/id_rsa
+# if [ ! -f ~/.ssh/id_rsa ]; then
+#     echo -e "\e[36mGenerating ssh keys...\e[0m"
+#     ssh-keygen -t rsa -b 4096 -C "gra@devcontainer" -f ~/.ssh/id_rsa -q -N ""
+#     echo -e "\e[36mSSH keys generated.\e[0m"
 # fi
+
+# Create devcontainer.metadata from template if it doesn't exist
+if [ ! -f ~/gra/.devcontainer/devcontainer.metadata ]; then
+    echo -e "\e[36mCreating devcontainer.metadata ...\e[0m"
+    cp ~/gra/.devcontainer/devcontainer.metadata_template ~/gra/.devcontainer/devcontainer.metadata
+fi
 
 source ~/.bashrc -i
 setdevmaster
