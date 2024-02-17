1bc2cb0cbb0e0a0defb614a523d8837150d7fefe
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 13:17:26 +0000

    Add sudo check

--- .devcontainer/native_install/native_install.sh
+++ .devcontainer/native_install/native_install.sh
@@ -6,8 +6,12 @@ export USERNAME=$USER
 # else
 #     sudo echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 # fi
+if  sudo true -neq 0; then 
+    echo "This script needs to be run as a regular account but it needs sudo for some commands."
+    exit 1;
+fi
 python3 .devcontainer/native_install/docker2sh.py --target gra-base .devcontainer/Dockerfile > installation_script_generated.sh
-cp .devcontainer/apt-get-wrapper.sh /usr/local/bin
+sudo cp .devcontainer/apt-get-wrapper.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/apt-get-wrapper.sh
 sudo chmod +x installation_script_generated.sh
 sudo ./installation_script_generated.sh
