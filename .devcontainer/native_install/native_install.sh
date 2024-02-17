52c4d39b805bb309fc1dce35ef10e355658f43b7
Daniel Bara <29439900+daniel-bara@users.noreply.github.com>
2024-02-17 03:30:01 +0000

    ask for sudo once

--- .devcontainer/native_install/native_install.sh
+++ .devcontainer/native_install/native_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+sudo echo "pi ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+python3 .devcontainer/native_install/docker2sh.py --target gra-base .devcontainer/Dockerfile > installation_script_generated.sh
+cp .devcontainer/apt-get-wrapper.sh /usr/local/bin
+sudo chmod +x /usr/local/bin/apt-get-wrapper.sh
+sudo chmod +x installation_script_generated.sh
+sudo ./installation_script_generated.sh
+
 mkdir -p $HOME/catkin_ws
 ./bot_scripts/install.sh
