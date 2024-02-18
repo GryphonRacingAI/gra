a3f776aead3aaeb2572a52fe14336f01792bee34
Daniel Bara <a@b>
2024-02-18 20:45:14 +0000

    Remove roscore service

--- bot_scripts/install.sh
+++ bot_scripts/install.sh
@@ -43,38 +43,4 @@ safe_symlink "$(realpath ~/gra/.devcontainer/.bashrc)" ~/.bashrc
 safe_symlink "$(realpath ~/gra/.devcontainer/convenience.sh)" ~/convenience.sh
 safe_symlink "$(realpath ~/gra/.devcontainer/bash_profile.sh)" ~/bash_profile.sh
 
-# Dynamically create the roscore.service file
-echo "Generating roscore.service for user $USER..."
-cat > roscore.service <<EOL
-[Unit]
-Description=roscore service
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/bin/bash -ic 'source ~/.bashrc && roscore'
-Restart=always
-User=$USER
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-# Check if roscore.service exists before copying
-if [ ! -f roscore.service ]; then
-    echo -e "${YELLOW}Warning: roscore.service does not exist in current directory. Skipping...${RESET}"
-else
-    echo "Copying roscore.service to /etc/systemd/system/"
-    sudo cp roscore.service /etc/systemd/system/
-
-    echo "Reloading systemd to recognize the service..."
-    sudo systemctl daemon-reload
-
-    echo "Enabling the roscore.service to start on boot..."
-    sudo systemctl enable roscore.service   
-
-    echo "Starting the roscore.service..."
-    sudo systemctl start roscore.service
-fi
-
 echo -e "${GREEN}Installation script finished.${RESET}"
