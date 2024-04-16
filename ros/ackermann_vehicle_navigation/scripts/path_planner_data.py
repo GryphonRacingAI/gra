321c02e4e4ee8a133ad9eb7ce3469bf12d657c3f
Kenneth L <86786290+ugsfume@users.noreply.github.com>
2024-04-16 22:55:57 +0100

    Path planner data preparation node tested

--- ros/ackermann_vehicle_navigation/scripts/path_planner_data.py
+++ ros/ackermann_vehicle_navigation/scripts/path_planner_data.py
@@ -2,62 +2,59 @@
 
 import rospy
 import numpy as np
+from geometry_msgs.msg import Point, Quaternion
 from std_msgs.msg import Header
-from geometry_msgs.msg import Point  # This might still be used for other purposes
-from ultralytics_ros.msg import ConeDetection, PathPlanningInput, Point2D  # Ensure Point2D is imported
+from ultralytics_ros.msg import ConeDetection, ExtendedConeDetection
 from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 class PathPlannerData:
     def __init__(self):
-        rospy.init_node('path_planner_data', anonymous=True)
+        rospy.init_node('path_planner_data', anonymous=False)
+
         # Subscribers
         self.cone_sub = rospy.Subscriber('/cone_list', ConeDetection, self.cone_callback)
         self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
-        # Publishers
-        self.path_input_pub = rospy.Publisher('/path_planning_input', PathPlanningInput, queue_size=10)
+
+        # Publisher
+        self.filtered_cones_pub = rospy.Publisher('/filtered_cone_list', ExtendedConeDetection, queue_size=10)
+
         # State
-        self.car_position = Point2D()
-        self.car_direction = Point2D(x=1.0, y=0.0)
-        self.cones_by_type = [[] for _ in range(5)]  # List of lists for each cone type
+        self.car_position = Point()
+        self.car_orientation = Quaternion()
+
+        print("path_planner_data node alive")
 
     def odom_callback(self, msg):
-        pos = msg.pose.pose.position
-        ori = msg.pose.pose.orientation
-        orientation_list = [ori.x, ori.y, ori.z, ori.w]
-        _, _, yaw = euler_from_quaternion(orientation_list)
-        # Update car position and direction
-        self.car_position.x = pos.x
-        self.car_position.y = pos.y
-        self.car_direction.x = np.cos(yaw)
-        self.car_direction.y = np.sin(yaw)
+        # print("Received odom data")
+        # Update car position
+        self.car_position = msg.pose.pose.position
+        self.car_orientation = msg.pose.pose.orientation
 
     def cone_callback(self, msg):
-        # Filter and categorize cones
-        cone_types = [msg.yellow_cones, msg.blue_cones, msg.orange_cones, msg.large_orange_cones, msg.unknown_cones]
-        cone_indices = [1, 2, 3, 4, 0]  # Mapping to fsd_path_planning indices
-        for idx, cones in zip(cone_indices, cone_types):
-            filtered_cones = []
+        # print("Received cone data")
+        filtered_cones = ExtendedConeDetection()
+        filtered_cones.header = Header(stamp=rospy.Time.now(), frame_id='path_planner')
+        filtered_cones.car_position = self.car_position
+        filtered_cones.car_orientation = self.car_orientation
+
+        # Filter cones based on distance
+        cone_lists = [msg.blue_cones, msg.yellow_cones, msg.orange_cones, msg.large_orange_cones, msg.unknown_cones]
+        filtered_lists = [[] for _ in cone_lists]
+
+        car_pos_array = np.array([self.car_position.x, self.car_position.y, self.car_position.z])
+        for i, cones in enumerate(cone_lists):
             for cone in cones:
-                distance = np.hypot(cone.x - self.car_position.x, cone.y - self.car_position.y)
-                if distance <= 10.0:  # Filter cones more than 10 meters away
-                    filtered_cones.append(Point2D(x=cone.x, y=cone.y))
-            self.cones_by_type[idx] = filtered_cones
+                cone_array = np.array([cone.x, cone.y, cone.z])
+                if np.linalg.norm(cone_array - car_pos_array) <= 10.0:
+                    filtered_lists[i].append(cone)
 
-        self.publish_path_planning_input()
+        # Assign filtered lists back to the message
+        filtered_cones.blue_cones, filtered_cones.yellow_cones, filtered_cones.orange_cones, \
+        filtered_cones.large_orange_cones, filtered_cones.unknown_cones = filtered_lists
 
-    def publish_path_planning_input(self):
-        msg = PathPlanningInput()
-        msg.header = Header(stamp=rospy.Time.now(), frame_id='path_planner')
-        msg.car_position = self.car_position
-        msg.car_direction = self.car_direction
-        # Assigning cone positions for each type
-        msg.yellow_cones = self.cones_by_type[1]
-        msg.blue_cones = self.cones_by_type[2]
-        msg.orange_small_cones = self.cones_by_type[3]
-        msg.orange_big_cones = self.cones_by_type[4]
-        msg.unknown_cones = self.cones_by_type[0]
-        self.path_input_pub.publish(msg)
+        self.filtered_cones_pub.publish(filtered_cones)
+        # print("Published filtered data")
 
 if __name__ == '__main__':
     try:
