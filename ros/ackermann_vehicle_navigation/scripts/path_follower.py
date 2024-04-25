c14c9c34ac209ed37bb9a3ea64f2523a27057c26
Kenneth L <86786290+ugsfume@users.noreply.github.com>
2024-04-25 14:46:17 +0100

    speed limit changed

--- ros/ackermann_vehicle_navigation/scripts/path_follower.py
+++ ros/ackermann_vehicle_navigation/scripts/path_follower.py
@@ -20,8 +20,8 @@ seq = 0
 start_time = rospy.Time()
 end_time = rospy.Time()
 
-yaw_rate_limit = True
-vx_limit = True
+yaw_rate_limit = False
+vx_limit = False
 
 def get_distance(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
@@ -82,13 +82,14 @@ def odom_callback(odom_msg):
         pre_tiem = current_time
     
         # yaw_rate = beta*1.0 + cte*1.00 + beta_dot*1.0 + cte_dot*0.05
-        yaw_rate = beta*0.7 + cte*0.5 + heading_error*1.0
+        # yaw_rate = beta*0.7 + cte*0.5 + heading_error*1.0
+        yaw_rate = beta*0.9 + cte*1.0 + heading_error*1.2  # Adjusted for more responsiveness
         # yaw_rate = beta*0.8 + cte*0.6
         if yaw_rate_limit is True:
             yaw_rate = max(min(yaw_rate, 0.5), -0.5)
 
         # vx = 0.6
-        vx = 0.7 - abs(beta*0.7 + cte*0.7 + heading_error*0.7)
+        vx = 2.5 - abs(beta*0.7 + cte*0.7 + heading_error*0.7)
         if vx_limit is True:
             vx = max(min(vx, 0.7), 0.1)
         rospy.loginfo(vx)
