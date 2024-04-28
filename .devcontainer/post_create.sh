d341828243f76e1f372b9b1bd1bf503b75d00a28
Kenneth L <86786290+ugsfume@users.noreply.github.com>
2024-04-28 04:56:36 +0100

    test

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,10 +4,46 @@ if [ ! -d "$HOME/catkin_ws/src" ]; then
     ln -s ~/gra/ros ~/catkin_ws/src
 fi
 
-cd ~/catkin_ws/src
-git clone https://github.com/koide3/ndt_omp.git
-git clone https://github.com/SMRT-AIST/fast_gicp.git --recursive
-git clone https://github.com/koide3/hdl_graph_slam
+# 3D pointcloud slam
+# cd ~/catkin_ws/src
+# git clone https://github.com/koide3/ndt_omp.git
+# git clone https://github.com/SMRT-AIST/fast_gicp.git --recursive
+# git clone https://github.com/koide3/hdl_graph_slam
+
+# # VSLAM
+# cd ~
+# git clone https://github.com/RainerKuemmerle/g2o.git
+# cd g2o
+# mkdir build
+# cd build
+# cmake ..
+# make
+
+# cd ~
+# git clone https://github.com/stella-cv/FBoW.git
+# cd FBoW && mkdir build && cd build
+# cmake .. -DBUILD_TESTS=ON -DBUILD_UTILS=ON
+# make
+
+mkdir -p ~/lib
+cd ~/lib
+git clone --recursive --depth 1 https://github.com/stella-cv/stella_vslam.git
+rosdep install -y -i --from-paths ~/lib
+cd ~/lib/stella_vslam
+mkdir -p ~/lib/stella_vslam/build
+cd ~/lib/stella_vslam/build
+source /opt/ros/${ROS_DISTRO}/setup.bash
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+make -j
+sudo make install
+
+cd ~
+git clone --recursive -b ros https://github.com/stella-cv/stella_vslam_ros.git
+cd stella_vslam_ros
+git submodule update --init --recursive
+ln -s ~/stella_vslam_ros ~/catkin_ws/src/stella_vslam_ros
+
+
 
 # # Generate ssh keys if they don't exist at ~/.ssh/id_rsa
 # if [ ! -f ~/.ssh/id_rsa ]; then
