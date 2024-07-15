9381097898e20e75b96067e1912ba7c3bfbd20d7
Kenneth L <86786290+ugsfume@users.noreply.github.com>
2024-07-15 03:25:05 +0100

    increased skidpad path follower sensitivity

--- ros/ackermann_vehicle_navigation/scripts/skidpad_path_follower.py
+++ ros/ackermann_vehicle_navigation/scripts/skidpad_path_follower.py
@@ -88,13 +88,13 @@ def odom_callback(odom_msg):
         # Heading Error is the difference between the vehicle's current heading and the desired heading, which is the direction from the current waypoint to the next waypoint. 
         # yaw_rate = beta*1.0 + cte*1.00 + beta_dot*1.0 + cte_dot*0.05
         # yaw_rate = beta*0.7 + cte*0.5 + heading_error*1.0
-        yaw_rate = beta*0.5 + cte*0.5 + heading_error*1  # Adjusted for more responsiveness
+        yaw_rate = beta*1.5 + cte*0.5 + heading_error*2.0  # Adjusted for more responsiveness
         # yaw_rate = beta*0.8 + cte*0.6
         if yaw_rate_limit is True:
             yaw_rate = max(min(yaw_rate, 0.366519), -0.366519)  # Limit steering angle to -21deg to 21deg, steering range of ADS-DV
 
         # vx = 0.6
-        vx = 3 - abs(beta*0.1 + cte*0.3 + heading_error*0.3)
+        vx = 3 - abs(beta*0.1 + cte*0.3 + heading_error*1.5)
         if vx_limit is True:
             # vx = max(min(vx, 0.7), 0.1)
             vx = max(vx, 0.1)
