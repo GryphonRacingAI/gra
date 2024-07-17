03fbe7e4e15d58d5d9034f5380c9f43fe6acbed7
Kenneth L <86786290+ugsfume@users.noreply.github.com>
2024-07-17 22:26:27 +0100

    autonomous demo now work in simulation
    traverse is done autonomously compared to previous implementation

--- ros/ackermann_vehicle_navigation/scripts/path_follower.py
+++ ros/ackermann_vehicle_navigation/scripts/path_follower.py
@@ -2,9 +2,7 @@
 import rospy
 from nav_msgs.msg import Path
 from nav_msgs.msg import Odometry
-from std_msgs.msg import Header
-from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import Pose
+from std_msgs.msg import Bool
 from geometry_msgs.msg import Twist
 import math
 import numpy as np
@@ -16,6 +14,7 @@ pre_beta = 0
 pre_cte = 0
 pre_time = 0
 seq = 0
+pause = True  # Initialize with pause
 
 start_time = rospy.Time()
 end_time = rospy.Time()
@@ -27,7 +26,7 @@ def get_distance(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 def odom_callback(odom_msg):
-    global subscribed_path, wp_index, pre_beta, pre_cte, pre_time, seq, end_time, start_time
+    global subscribed_path, wp_index, pre_beta, pre_cte, pre_time, seq, end_time, start_time, pause
     if len(subscribed_path.poses) != 0:
         # Robot's current X, Y point
         current_point = np.array([odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y])
@@ -46,7 +45,6 @@ def odom_callback(odom_msg):
         next_point_distance = np.linalg.norm(next_wp - current_point)
         if next_point_distance < 0.4:
             wp_index = wp_index+1
-            # rospy.loginfo("length:{} wp_index:{}".format(len(subscribed_path.poses), wp_index))
             if wp_index >= len(subscribed_path.poses):
                 end_time = rospy.Time.now()
                 rospy.loginfo((end_time - start_time).to_sec())
@@ -67,8 +65,8 @@ def odom_callback(odom_msg):
         if np.cross(ba, bc) > 0:
             position = -1
 
-        beta = position*np.arccos(cosine_angle)
-        cte = position*np.linalg.norm(np.cross(bc, -ba))/np.linalg.norm(bc)
+        beta = position * np.arccos(cosine_angle)
+        cte = position * np.linalg.norm(np.cross(bc, -ba)) / np.linalg.norm(bc)
     
         current_time = rospy.Time.now()
         if seq == 0:
@@ -76,29 +74,22 @@ def odom_callback(odom_msg):
             pre_time = rospy.Time.now() - rospy.Duration(0.001)
 
         dt = (current_time - pre_time).to_sec()
-        # rospy.loginfo(dt)
         beta_dot = (beta - pre_beta) / dt
         cte_dot = (cte - pre_cte) / dt
-        pre_tiem = current_time
+        pre_time = current_time
 
-        # beta is the angle between the vehicle's heading and the line connecting the vehicle's current position to the next waypoint in the path. This angle helps determine how sharply the vehicle needs to turn towards the waypoint.
-        # cte(Cross Track Error) is the shortest perpendicular distance from the vehicle to the path line between the current waypoint and the next waypoint. This error indicates how far off the path the vehicle is.
-        # Heading Error is the difference between the vehicle's current heading and the desired heading, which is the direction from the current waypoint to the next waypoint. 
-        # yaw_rate = beta*1.0 + cte*1.00 + beta_dot*1.0 + cte_dot*0.05
-        # yaw_rate = beta*0.7 + cte*0.5 + heading_error*1.0
-        yaw_rate = beta*0.8 + cte*0.5 + heading_error*1.5  # Adjusted for more responsiveness
-        # yaw_rate = beta*0.8 + cte*0.6
-        if yaw_rate_limit is True:
+        yaw_rate = beta * 0.5 + cte * 0.5 + heading_error * 1  # Adjusted for more responsiveness
+        if yaw_rate_limit:
             yaw_rate = max(min(yaw_rate, 0.366519), -0.366519)  # Limit steering angle to -21deg to 21deg, steering range of ADS-DV
 
-        # vx = 0.6
-        vx = 3 - abs(beta*0.1 + cte*0.3 + heading_error*1.5)
-        if vx_limit is True:
-            # vx = max(min(vx, 0.7), 0.1)
-            vx = max(vx, 0.1)
-            pass
+        if pause:
+            vx = 0
+        else:
+            vx = 4.5 - abs(beta * 0.1 + cte * 0.3 + heading_error * 0.3)
+            if vx_limit:
+                vx = max(vx, 0.1)
+
         rospy.loginfo(vx)
-        # rospy.loginfo("wp_index:{} heading_error:{} heading:{} wp_heading:{} beta:{} cte:{} yaw_rate:{} vx:{}".format(subscribed_path.poses[wp_index-1], heading_error, current_heading, wp_heading, np.degrees(beta), cte, yaw_rate, vx))
         pre_beta = beta
         pre_cte = cte
         cmd_vel_msg = Twist()
@@ -112,11 +103,16 @@ def path_callback(path_msg):
     global subscribed_path
     subscribed_path = path_msg
 
+def pause_callback(pause_msg):
+    global pause
+    pause = pause_msg.data
+
 if __name__ == '__main__':
     rospy.init_node('path_follower', anonymous=True, disable_signals=True)
     cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     rospy.Subscriber('/path', Path, path_callback)
     rospy.Subscriber('/odom', Odometry, odom_callback, queue_size=1)
+    rospy.Subscriber('/pause', Bool, pause_callback)
 
     try:
         rospy.spin()
--- ros/fsai_mission/scripts/autonomous_demo.py
+++ ros/fsai_mission/scripts/autonomous_demo.py
@@ -4,38 +4,33 @@ import time
 import rospy
 from std_msgs.msg import Bool
 from ackermann_msgs.msg import AckermannDrive
-from fsai_api.msg import VCU2AI
 
 class AutonomousDemonstration:
     WHEEL_CIRCUMFERENCE = 1.64
+
     def __init__(self):
-        rospy.init_node('static_inspection_A', anonymous=False)
+        rospy.init_node('autonomous_demo', anonymous=False)
         self.ackermann_publisher = rospy.Publisher('/ackermann_cmd_controller', AckermannDrive, queue_size=1)
         self.brake_publisher = rospy.Publisher('/brake', Bool, queue_size=1)
         self.emergency_brake_publisher = rospy.Publisher('/emergency_brake', Bool, queue_size=1)
         self.chequered_flag_publisher = rospy.Publisher('/chequered_flag', Bool, queue_size=1)
-        rospy.Subscriber('/vcu2ai', VCU2AI, self.vcu2ai_callback)
+        self.pause_publisher = rospy.Publisher('/pause', Bool, queue_size=1)
         self.rate = rospy.Rate(10)  # 10 Hz
-
-        self.rl_wheel_speed_rpm = None
-        self.rr_wheel_speed_rpm = None
-        self.rr_pulse_count = None
-        self.rl_pulse_count = None
-
-    def vcu2ai_callback(self, msg):
-        self.rl_wheel_speed_rpm = msg.rl_wheel_speed_rpm
-        self.rr_wheel_speed_rpm = msg.rr_wheel_speed_rpm
-        self.rr_pulse_count = msg.rr_pulse_count
-        self.rl_pulse_count = msg.rl_pulse_count
 
     def start(self):
         time.sleep(1)
+        self.pause_publisher.publish(Bool(data=True))
         self.sweep_steering()
         time.sleep(1)
-        self.accelerate_10m()
+        self.pause_publisher.publish(Bool(data=False))
+        time.sleep(5)
+        self.pause_publisher.publish(Bool(data=True))
         self.brake_10m()
-        self.accelerate_10m()
+        time.sleep(5)
+        self.pause_publisher.publish(Bool(data=False))
+        time.sleep(5)
         self.emergency_brake()
+        rospy.loginfo("Autonomous demo routine completed.")
 
     def sweep_steering(self):
         rospy.loginfo("Starting steering sweep")
@@ -61,39 +56,18 @@ class AutonomousDemonstration:
             self.ackermann_publisher.publish(ackermann_message)
             rospy.loginfo(f"Set steering angle to {angle}")
             self.rate.sleep()
-        rospy.loginfo(f"Steering demo done")
-        
+        rospy.loginfo("Steering demo done")
 
-    def accelerate_10m(self):
-        rospy.loginfo("Starting drivetrain ramp up")
-        assert self.rl_pulse_count is not None
-        assert self.rr_pulse_count is not None
-        start_stopwatch = time.time()
-        ACCELERATION = 1.5 # m/s^2
-        initial_rl = self.rl_pulse_count
-        initial_rr = self.rr_pulse_count
-        pulse_count_10m = 10/self.WHEEL_CIRCUMFERENCE*20 # 10m / (WHEEL_CIRCUMFERENCE m / rotation) * 20 (pulse/rotation)
-
-        while not rospy.is_shutdown() and (self.rl_pulse_count < initial_rl + pulse_count_10m) and (self.rr_pulse_count < initial_rr+pulse_count_10m):
-            ackermann_message = AckermannDrive()
-            elapsed = time.time() - start_stopwatch
-            ackermann_message.speed = elapsed * ACCELERATION
-            rospy.loginfo(f"Actual speed: {self.rr_wheel_speed_rpm}, {self.rr_wheel_speed_rpm}")
-            self.ackermann_publisher.publish(ackermann_message)
-            self.rate.sleep()
-            if elapsed > 10:
-                break
-    
     def brake_10m(self):
-        ackermann_message = AckermannDrive()
-        self.ackermann_publisher.publish(ackermann_message)
-        while not rospy.is_shutdown() and self.rl_wheel_speed_rpm > 5:
-            self.brake_publisher.publish(True)
-            self.rate.sleep()
-        self.brake_publisher.publish(False)
+        self.brake_publisher.publish(Bool(data=True))
+        rospy.loginfo("Braking for 5 seconds")
+        time.sleep(5)
+        self.brake_publisher.publish(Bool(data=False))
+        rospy.loginfo("Braking complete")
 
     def emergency_brake(self):
-        self.emergency_brake_publisher.publish(True)
+        self.emergency_brake_publisher.publish(Bool(data=True))
+        rospy.loginfo("Emergency brake applied")
 
     @staticmethod
     def linear_interpolate(value, a1, a2, b1, b2):
@@ -109,7 +83,7 @@ class AutonomousDemonstration:
 
 if __name__ == '__main__':
     try:
-        inspection = AutonomousDemonstration()
-        inspection.start()
+        demo = AutonomousDemonstration()
+        demo.start()
     except rospy.ROSInterruptException:
         pass
