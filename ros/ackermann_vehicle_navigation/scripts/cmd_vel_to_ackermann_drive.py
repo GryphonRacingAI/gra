ca5681d7604e2b8ec33dcbc20e9caeba1ff3df9f
Kenneth LKH <86786290+ugsfume@users.noreply.github.com>
2024-08-01 13:28:42 +0100

    Update ros/ackermann_vehicle_navigation/scripts/cmd_vel_to_ackermann_drive.py
    Co-authored-by: Daniel Bara <29439900+daniel-bara@users.noreply.github.com>

--- ros/ackermann_vehicle_navigation/scripts/cmd_vel_to_ackermann_drive.py
+++ ros/ackermann_vehicle_navigation/scripts/cmd_vel_to_ackermann_drive.py
@@ -5,7 +5,7 @@
 # if it does not work - please change to '#!/usr/bin/env python' in path on the header
 # This is to fix some compatibility issues with ROS on Melodic
 
-# This node convert the standard /vel_cmd to ackermann specific transformation commands /ackermann_cmd
+# This node converts the standard /cmd_vel to ackermann specific drive commands /ackermann_cmd
 
 import rospy, math
 from geometry_msgs.msg import Twist
